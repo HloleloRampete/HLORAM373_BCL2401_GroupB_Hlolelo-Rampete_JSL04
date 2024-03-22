a88371f32f91c185fc462859423a9ee53ec11177
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-03-22 15:52:53 +0200

    comments

--- index.js
+++ index.js
@@ -2,33 +2,33 @@
 
 // #### Challenge 1: Flavor Selection
 // Given a variable that holds a user's preferred type of Monster Energy drink, 
-// print whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
+// display whether the user prefers a "Regular" or "Sugar-free" variety. If the preference is not set, default to "Regular".
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
 
 // Determine the user's preferred Monster Energy flavor, defaulting to 'Regular' if not explicitly set
-// Print the variety based on user's preference
+// Display the variety based on user's preference
 console.log(`${userPreference === 'Sugar-free' ? 'Sugar-free' : 'Regular'}`);
 
 // #### Challenge 2: Stock Check
 // You have a variable that tracks the number of Monster Energy cans left in the fridge. 
-// Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
+// Use a ternary operator to display "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
-let cansLeft = 3; // Number of cans
+let cansLeft = 4; // Number of cans
 
-// Check the stock status based on the number of cans left
-// Print the stocking status based on the remaining cans on stock.
+/* Check the stock status against the number of cans left
+ Display the stocking status based on the remaining cans on stock. */
 console.log(`${cansLeft < 5 ? "Time to restock!" : "We're stocked!"}`);
 
 
 // #### Challenge 3: Workout Intensity
 // Based on the current heart rate, determine if the user should drink a Monster Energy drink to boost their workout. 
-// If the heart rate is below 100 bpm, print "Boost needed!", otherwise, print "Energy levels are high!".
+// If the heart rate is below 100 bpm, display "Boost needed!", otherwise, print "Energy levels are high!".
 
-let heartRate = 95; // Current heart rate in bpm
+let heartRate = 94; // Current heart rate in bpm
 
-// Evaluate energy levels based on the current heart rate
-// Print an appropriate advise based on the current heart rate
+/* Check energy levels based on the current heart rate
+ Display an appropriate advise based on the current heart rate */
 console.log(`${heartRate < 100 ? 'Boost needed!' : 'Energy levels are high!'}`);
 
 
@@ -36,10 +36,10 @@ console.log(`${heartRate < 100 ? 'Boost needed!' : 'Energy levels are high!'}`);
 // Monster Energy drinks are best served cold. Given the current temperature, use a ternary to print "Chilled to perfection!" 
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
-let currentTemp = 4; // Current temperature in °C
+let currentTemp = 3; // Current temperature in °C
 
-// Assess temperature suitability for the Monster Energy drink
-// Print a particular suggestion based on the temperature
+/* Examine the temperature suitability for the Monster Energy drink
+ Display a particular suggestion based on the temperature */
 console.log(`${currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!'}`);
 
 
@@ -49,14 +49,14 @@ console.log(`${currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!'}`
 // otherwise, print "Better stick to water."
 
 // Define the current hour in 24-hour format
-let currentHour = 22;  // Current hour in 24-hour format
+let currentHour = 20;  
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
 // Otherwise, print "Better stick to water."
 
 // Recommend whether a Monster Energy drink is appropriate based on the current hour
-// Print the appropriate recommendation
+// Display the appropriate recommendation
 console.log(`${currentHour >= 7 && currentHour < 24 ? 'Unleash the beast!' : 'Better stick to water.'}`);
 
 
